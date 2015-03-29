1ce3ed0208703cdcb9ccdab364f2ed4808931c9a
anthonybrice <anthony.brice423@myci.csuci.edu>
2015-03-28 17:59:02 -0700

    adding quake-style terminal

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,9 +1,12 @@
+-- xmonad.hs
+
 import           XMonad
 import           XMonad.Hooks.DynamicLog
 import           XMonad.Hooks.ManageDocks
 -- import XMonad.Layout.NoBorders
 -- import XMonad.Util.Run(spawnPipe)
 import           XMonad.Util.EZConfig       (additionalKeysP)
+import           XMonad.Util.Scratchpad -- For quake-like terminal
 -- import System.IO
 -- import XMonad.Util.Dmenu
 -- import Graphics.X11.ExtraTypes.XF86
@@ -19,6 +22,9 @@ import           Control.Applicative        ((<$>))
 import           XMonad.Hooks.UrgencyHook
 import           XMonad.Util.NamedWindows
 import           XMonad.Util.Run
+
+-- For resizable windows
+import XMonad.Layout.ResizableTile
 
 main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
 
@@ -38,21 +44,24 @@ currentWindowColor = "#aa2e00"
 
 -- Custom PP, configure it as you like. It determines what is being written to the bar.
 myPP = xmobarPP { ppCurrent = xmobarColor currentWindowColor "" . wrap "<" ">"
-                , ppTitle = xmobarColor currentWindowColor "" . shorten 72
+                , ppTitle = xmobarColor currentWindowColor "" . shorten 62
                 , ppSep = " | "
                 }
 
 -- Key bindings to toggle the gap for the bar.
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
---myLayoutHook = Full ||| noBorders Full
+myLayoutHook = ResizableTall 1 (3/100) (1/2) [] ||| Tall 1 (3/100) (1/2)
+               ||| Mirror (Tall 1 (3/100) (1/2)) ||| Full
+
+myTerminal = "urxvt"
 
 -- Main configuration, override the defaults to your liking
 myConfig = withUrgencyHook LibNotifyUrgencyHook $ defaultConfig
-  { terminal = "gnome-terminal" -- urxvt config in ~/.Xresources
+  { terminal = myTerminal -- urxvt config in ~/.Xresources
   , manageHook = myManageHooks
   , startupHook = setWMName "LG3D"
-  , layoutHook = smartBorders $ avoidStruts $ layoutHook defaultConfig
+  , layoutHook = smartBorders $ avoidStruts $ myLayoutHook
   , focusedBorderColor = currentWindowColor
   , logHook = dynamicLogWithPP myPP
   , modMask = mod4Mask -- Rebind Mod to the Windows key
@@ -67,6 +76,9 @@ myConfig = withUrgencyHook LibNotifyUrgencyHook $ defaultConfig
              ++ " -sf black -sb '#e1f1f6' -fn"
              ++ " 'inconsolatazi4-13:antialias=true:bold'"
              ++ " ` && eval \"exec $exe\"")
+  , ("M4-a", sendMessage MirrorShrink)
+  , ("M4-z", sendMessage MirrorExpand)
+  , ("M4-`", scratchpadSpawnActionTerminal myTerminal)
   , ("<XF86AudioRaiseVolume>", spawn "amixer set Master 10%+")
   , ("<XF86AudioLowerVolume>", spawn "amixer set Master 10%-")
   , ("<XF86AudioMute>", spawn "amixer set Master toggle")
@@ -76,12 +88,17 @@ myConfig = withUrgencyHook LibNotifyUrgencyHook $ defaultConfig
 myManageHooks = composeAll
   [ isFullscreen --> doFullFloat
   , isDialog --> doF W.shiftMaster <+> doF W.swapDown
-  -- , className =? "NetBeans IDE 8.0" --> doFloat
-  -- , className =? "MATLAB" --> doFloat
-  -- , className =? "com-mathworks-util-PostVMInit" --> doFloat
-  -- , className =? "jetbrains-idea" --> doFloat
   , manageDocks
+  , manageScratchPad
   ]
+
+manageScratchPad :: ManageHook
+manageScratchPad = scratchpadManageHook (W.RationalRect l t w h)
+  where
+    h = 0.1 -- terminal height, 10%
+    w = 1 -- terminal width, 100%
+    t = 1 - h -- distance from top edge 90%
+    l = 1 - w -- distance from left edge, 0%
 
 -- myLogHook h = dynamicLogWithPP $ xmobarPP
 --               { ppTitle = xmobarColor "green" "" . shorten 50
